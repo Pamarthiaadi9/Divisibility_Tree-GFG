7358f43137a7394ab8f27cc4490debd030f5a6dd
Aadi Siva Kartheek Pamarthi <105631285+Pamarthiaadi9@users.noreply.github.com>
2024-05-16 11:52:22 +0530

    Create dt.py

---
+++ dt.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def __init__(self):
+        self.ans = 0
+        
+    def dfs(self, adj: List[List[int]], vis: List[int], start: int) -> int:
+        vis[start] = 1
+        cnt = 0
+        
+        for it in adj[start]:
+            if not vis[it]:
+                res = self.dfs(adj, vis, it)
+                if res % 2 == 0:
+                    self.ans += 1
+                else:
+                    cnt += res
+        
+        return cnt + 1
+
+    def minimumEdgeRemove(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        for edge in edges:
+            adj[edge[0] - 1].append(edge[1] - 1)
+            adj[edge[1] - 1].append(edge[0] - 1)
+        
+        vis = [0] * n
+        self.dfs(adj, vis, 0)
+        
+        return self.ans
+
+
